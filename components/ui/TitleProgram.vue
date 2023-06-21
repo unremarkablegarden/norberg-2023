f14024b61694914fb0ec419e99b4567b23ee8174
unremarkablegarden <olle@unremarkablegarden.com>
2023-06-21 12:59:16 +0200

    bättre program

---
+++ components/ui/TitleProgram.vue
@@ -0,0 +1,110 @@
+<template lang="pug">
+h3.title.flex.text-green(:class="{ 'text-lg': text.length <= 16, 'text-base': text.length >= 16 }")
+  //- .decoration.left(:style='{ "width": d().wl, "background-size": "auto " + d().sl }')
+  //- .left-svg
+    img(
+      src='/gfx/artist-name-box_left.svg'
+      :height="height1 + 'px'"
+      :width="width1 + 'px'"
+    )
+  .label(ref='label').bg-black
+    slot
+    //- span : {{ d().sl }}
+  //- .decoration.right
+  //- .right-svg
+    img(src='/gfx/artist-name-box_right.svg')
+</template>
+
+<script setup>
+const props = defineProps({
+  text: String,
+  duration: Number
+})
+
+// const label = ref(null)
+// const height1 = ref(22)
+// const width1 = ref(22)
+
+// onMounted(() => {
+//   const getHeight = () => {
+//     let l = label.value
+//     let h = l.clientHeight
+//     return h
+//   }
+//   height1.value = getHeight()
+//   width1.value = getHeight()
+// })
+
+
+// const d = () => {
+//   const l = props.text.length
+  
+//   // one row
+//   let sl = 1.4
+//   let wl = 0.5
+//   let sr = 1.4
+//   let wr = 0.7
+  
+//   // two rows
+//   if (l > 12 && l < 20) { 
+//     sl = 2.8
+//     wl = 1.2
+//     sr = 3
+//     wr = 1.9
+//   }
+  
+//   return {
+//     sl: `${sl}rem`,
+//     wl: `${wl}rem`,
+//     sr: `${sr}rem`,
+//     wr: `${wr}rem`
+//   }
+// }
+
+
+</script>
+
+<style lang="sass" scoped>
+.label
+  // padding: 0 0.3rem 0 0.3rem
+  padding: 0 0.5rem 0 0.3rem
+  width: min-content
+// .decoration
+//   $sl: 1.4rem
+//   $wl: 0.5rem
+//   $sr: 1.4rem
+//   $wr: 0.7rem
+  
+//   // $sl: 2.8rem
+//   // $wl: 1.2rem
+//   // $sr: 3rem
+//   // $wr: 1.9rem
+  
+//   background-repeat: no-repeat
+//   display: inline-block
+//   background-color: transparent
+//   // box-shadow: 0 0 0 1px yellow
+//   &.left
+//     background-size: auto $sl
+//     width: $wl
+//     background-position-x: right
+//     transform: translateX(2px)
+//     background-image: url(/gfx/artist-name-box_left.svg)
+//   &.right
+//     background-size: auto $sr
+//     width: $wr
+//     // background-size: auto 2.4rem
+//     // width: 2rem
+//     transform: translateX(-2px)
+//     background-image: url(/gfx/artist-name-box_right.svg)
+//   &.left-svg
+//   &.right-svg
+    
+  // &.orange-black
+  //   .left
+  //     background-image: url(/gfx/button-side-fill-left.svg)
+  //   .right
+  //     background-image: url(/gfx/button-side-fill-right.svg)
+  
+  
+</style>
--- pages/program/[[slug]].vue
+++ pages/program/[[slug]].vue
@@ -13,32 +13,29 @@ SeoMeta(title='Program')
     .days-menu.md_w-1x2.flex.md_mb-16.md_pb-8.md_justify-end
       .day(
         v-for='(d, i) in datesObjects' :key='i' 
-        @click='selectedDay = d.date' 
+        @click='setDay(d.date)' 
       )
-        template(v-if='d.date !== "July 9"')
-          .title.cursor-pointer.mx-3.md_ml-6.md_mr-0
-            img(
-              :src='"gfx/button_date-tab-"+(i+1)+"-" + (selectedDay === d.date ? "active" : "inactive") + ".svg"'
-              :style='{ "width": buttonWidth + "px" }'
-            )
+          //- nuxt-link(:to='"/program/" + slugify(d.date)')
+          
+          template(v-if='d.date !== "July 9"')
+            .title.cursor-pointer.mx-3.md_ml-6.md_mr-0
+              img(
+                :src='"/gfx/button_date-tab-"+(i+1)+"-" + (selectedDay === d.date ? "active" : "inactive") + ".svg"'
+                :style='{ "width": buttonWidth + "px" }'
+              )
 
   .days
     .day(v-for='(d, i) in datesObjects' :key='i', :class='{ "hidden": selectedDay !== d.date }')
       
-      //- .locations.flex.w-full.bg-orange.pt-3.border-2.border-darkpurple
       .locations.md_flex.w-full.bg-orangeX.pt-3
         .location(v-for='(l, i2) in locations').md_w-1x4.border-darkpurple
       
-          //- h3.font-a.text-2xl.pb-3.text-center.border-b-2.border-darkpurple {{ l }}
-          //- h3.font-a.text-2xl.pb-3.text-center.border-b-4.border-black {{ l }}
           .title.text-2xl.text-center.relative.mb-6.mt-8._md_mt-0.sticky.top-24.md_top-48.z-50.Xbg-orange
             UiPageTitle(:width='pageWidth/div') {{ l }}
           
-          //- .events.border-darkpurple(
           .events.border-black.Xmd_border-r.Xmd_border-l.border-t-4.border-b(
             class='md_h-[1386px]'
           )
-            //- :class='{ "border-l-2": i2 > 0, "pl-[2px]": i2 === 0 }'
 
             .event.md_absolute.z-20(
               v-for='(e, j) in d.locations.find(loc => loc.location === l)?.events' 
@@ -52,93 +49,49 @@ SeoMeta(title='Program')
                   :style='{ "height": e.data?.duration ? (e.data?.duration * programHeightMult) + "px" : 70 + "px", "width": ((pageWidth) / (div + (div == 1 ? .01 : .02))) + "px", "margin-top": (e.data?.timeOffset * programHeightMult) + "px", "background-image": "url("+e.data?.artists?.[0]?.artist?.data?.image?.url+")" }'
                   :class='{ "bg-lightgrey": ! e.data?.duration }'
                 )
-                  //- .image.absolute.h-20.w-20
-                  //-   prismic-image(
-                  //-     :field="e.data?.artists?.[0]?.artist?.data?.image"
-                  //-     :imgix-params="{ w: 400, h: 400, fit: 'facearea', facepad: 10, faceindex: 1 }"
-                  //-   )
                   .meta.text-sm.mb-1
                     span.start {{ e.data?.hour }}:{{ e.data?.minute }} — 
                     span.end {{ e.data?.endTime }}
                     span(v-if='!e.data.duration') —Duration undetermined
                     
-                  //- span.text-lg.mr-1 {{ $prismic.asText(e.data?.title) }}
-                  .text-green.text-xl.md_text-2xl.whitespace-nowrap.mr-2.scale-75.origin-left
-                    UiTitleMarquee(:purple='false').text-xl.md_text-2xl {{ $prismic.asText(e.data?.title) }}
+                  .mr-2
+                    UiTitleProgram(:text='$prismic.asText(e.data?.title)' :duration='e.data?.duration') {{ $prismic.asText(e.data?.title) }}
                   
                   div.text-sm.uppercase.mt-1 {{ e.data?.type }}
                   
                   pre(v-if='e.data?.offsite') {{ e.data.offsite }}
                   pre(v-if='e.data?.google_map && e.data?.google_map.link_type !== "Any"') {{ e.data.google_map }}
                   pre(v-if='e.data?.info?.length') {{ e.data.info }}
-
-              //- .px-3.py-3.border-t-2.border-b-2.border-black.bg-orange(
-              //-   :style='{ "height": e.data?.duration ? (e.data?.duration * programHeightMult) + "px" : 70 + "px", "width": (pageWidth / 4) -3 + "px", "margin-top": (e.data?.timeOffset * programHeightMult) + "px" }'
-              //-   :class='{ "bg-purple": ! e.data?.duration }'
-              //- )
-                //- .font-d.border-2.border-darkgreen.bg-green.overflow-hidden.px-3.py-3.hover_border-darkpurple.hover_bg-purple(
-                //-   :style='{ "height": e.data?.duration ? (e.data?.duration * 1.5) + "px" : 70 + "px", "width": (pageWidth / 4) -3 + "px", "margin-top": (e.data?.timeOffset * 1.5) + "px" }'
-                //-   :class='{ "bg-purple": ! e.data?.duration }'
-                //- )
-                //-   .meta.text-sm.mb-1.opacity-50
-                //-     span.start {{ e.data?.hour }}:{{ e.data?.minute }} — 
-                //-     span.end {{ e.data?.endTime }}
-                    
-                //-   span.font-d.text-lg.mr-1 {{ $prismic.asText(e.data?.title) }}
-                //-   span.text-xs.opacity-50 {{ e.data?.type }}
-                  
-                //-   pre(v-if='e.data?.offsite') {{ e.data.offsite }}
-                //-   pre(v-if='e.data?.google_map && e.data?.google_map.link_type !== "Any"') {{ e.data.google_map }}
-                //-   pre(v-if='e.data?.info?.length') {{ e.data.info }}
-  
-  
     
   .bottom-spacer.h-32
               
 </template>
 
-<style lang="scss" scoped>
-.events {
-  // $s: 90px;
-  // $s: 120px;
-  $s: 138px;
-  background-size: $s $s;
-  background-image:
-    // linear-gradient(to right, grey 1px, transparent 1px),
-    linear-gradient(to bottom, #CA3702 1px, transparent 1px);
-}
-.shadow-border {
-  box-shadow: 0 0 0 1.5px black;
-}
-.bg-none {
-  background-image: none !important;
-}
-@media screen and (max-width: 640px) {
-  .program-box {
-    margin-top: 0 !important;   
-  }
-}
-.program-box {
-  background-repeat: no-repeat;
-  background-size: 0 0;
-  @media screen and (min-width: 640px) {
-    &:hover {
-      background-size: cover;
-      backdrop-filter: blur(2px);
-    }
-  }
-}
-
-</style>
 
 <script setup>
 definePageMeta({
   layout: "program"
 })
-
 const { client } = usePrismic()
 const data = reactive({})
+
+const slugify = (str) => str.toLowerCase().replace(/ /g, '-')
+const titleify = (str) => str.split('-').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+
 const selectedDay = ref('July 6')
+
+const { params } = useRoute()
+const slug = params.slug
+
+if (slug) selectedDay.value = titleify(slug)
+else selectedDay.value = 'July 6'
+
+const setDay = (date) => {
+  selectedDay.value = date
+  // update URL softly
+  window.history.pushState({}, '', `/program/${slugify(date)}`)
+}
+
 
 const { data: docs } = await useAsyncData('docs', () => client.getByType('program', 
 { pageSize: 100, fetchLinks: 'artist.image' }))
@@ -148,8 +101,8 @@ const locations = [
   'Kraftcentralen',
   '303',
   'Krossverket',
-  // 'Off-site',
-  // 'Radio'
+  'Off-site',
+  'Radio'
 ]
 
 
@@ -277,19 +230,21 @@ datesObjects.forEach((dateObject, i) => {
 
 
 // title stuff
-
-const pageWidth = ref(0)
+const pageWidth = ref(1000)
 const buttonWidth = ref(100)
 const programHeightMult = 2.3
-const div = ref(4)
+const div = ref(6)
 const titleEl = 'program'
 
 const resizeHandler = (titleEl) => {
-  pageWidth.value = document.getElementById(titleEl).offsetWidth
-  buttonWidth.value = pageWidth.value / 10
-  if (buttonWidth.value < 100) buttonWidth.value = 100
-  const tailwindSmBreakpoint = 640
-  if (pageWidth.value < tailwindSmBreakpoint) div.value = 1
+  const el = document.getElementById(titleEl)
+  if (el) {
+    pageWidth.value = el.offsetWidth
+    buttonWidth.value = pageWidth.value / 10
+    if (buttonWidth.value < 100) buttonWidth.value = 100
+    const tailwindSmBreakpoint = 640
+    if (pageWidth.value < tailwindSmBreakpoint) div.value = 1
+  }
 }
 
 onMounted(() => {
@@ -300,5 +255,39 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener('resize', () => resizeHandler())
 })
-
 </script>
+
+
+<style lang="scss" scoped>
+.events {
+  // $s: 90px;
+  // $s: 120px;
+  $s: 138px;
+  background-size: $s $s;
+  background-image:
+    // linear-gradient(to right, grey 1px, transparent 1px),
+    linear-gradient(to bottom, #CA3702 1px, transparent 1px);
+}
+.shadow-border {
+  box-shadow: 0 0 0 1.5px black;
+}
+.bg-none {
+  background-image: none !important;
+}
+@media screen and (max-width: 640px) {
+  .program-box {
+    margin-top: 0 !important;   
+  }
+}
+.program-box {
+  background-repeat: no-repeat;
+  background-size: 0 0;
+  @media screen and (min-width: 640px) {
+    &:hover {
+      background-size: cover;
+      backdrop-filter: blur(2px);
+    }
+  }
+}
+
+</style>
