de5e0023bf9437f036f114cdb2a2eed97322b341
unremarkablegarden <olle@unremarkablegarden.com>
2023-07-06 14:02:28 +0200

    radio

--- components/layout/SiteHeader.vue
+++ components/layout/SiteHeader.vue
@@ -1,72 +1,54 @@
-<template>
-  <header class="px-4 lg_px-10 2xl_px-16 bg-black z-30 absolute w-full left-0">
-    <div
-      class="bar-top uppercase text-primary font-d text-lg md_text-2xl leading-7 py-2"
-      id="header"
-    >
-      <div class="flex justify-between h-5">
-        <button
-          @click="toggleRadio"
-          class="uppercase left date flex gap-4 max-w-full md_max-w-2xl"
-        >
-          <span v-if="playing" class="flex gap-2">
-            <div class="bg-primary w-2 h-5"></div>
-            <div class="bg-primary w-2 h-5"></div
-          ></span>
-          <span v-else>▶</span>
-          <div
-            class="border-primary border rounded-full flex px-2 items-center justify-center h-6 gap-1 w-24 sm_w-20 mt-[-1px]"
-          >
-            <div class="h-2 w-2 animate-pulse bg-primary rounded-full"></div>
-            <span class="text-2xs mb-0 mt-1 w-fit">on air</span>
-          </div>
-          <Vue3Marquee pauseOnHover="1"
-            >Norbergfestival radio
-            <img class="h-8 mx-2 -mt-2" src="/star.png" />
-            <span v-if="data"> {{ data.current_track.title }}</span>
-            <span v-else> Loading</span>
-            <img class="h-8 mx-2 -mt-2" src="/star.png" />
-          </Vue3Marquee
-          >
-        </button>
-        <nuxt-link to="/artist/radio" class="right location text-right hidden md_block"
-          >Schedule →</nuxt-link
-        >
-      </div>
-    </div>
-  </header>
+<template lang="pug">
+header.px-4.lg_px-10.2xl_px-16.bg-black.z-30.absolute.w-full.left-0
+  .bar-top.uppercase.text-primary.font-d.text-lg.md_text-2xl.leading-7.py-2#header
+    .flex.justify-between.h-5
+      button.uppercase.left.date.flex.gap-4.max-w-full.md_max-w-2xl(@click='toggleRadio')
+        
+        span.flex.gap-2(v-if='playing')
+          .bg-primary.w-2.h-5
+          .bg-primary.w-2.h-5
+        span(v-else) ▶
+        .border-primary.border.rounded-full.flex.px-2.items-center.justify-center.h-6.gap-1.w-24.sm_w-20(class='mt-[-1px]')
+          .h-2.w-2.animate-pulse.bg-primary.rounded-full
+          span.text-2xs.mb-0.mt-1.w-fit on air
+        
+        Vue3Marquee(pauseonhover='1')
+          span Norbergfestival radio
+          img.h-8.mx-2.-mt-2(src='/star.png')
+          span(v-if='data') {{ data.current_track.title }}
+          span(v-else) Loading
+          img.h-8.mx-2.-mt-2(src='/star.png')
+      nuxt-link.location.hidden.md_block(to='/artist/radio') Schedule →
 </template>
 
 <script setup>
-import { Vue3Marquee } from "vue3-marquee";
-import "vue3-marquee/dist/style.css";
-let playing = ref(false);
-let radio = ref(null);
+import { Vue3Marquee } from "vue3-marquee"
+import "vue3-marquee/dist/style.css"
+let playing = ref(false)
+let radio = ref(null)
 const { pending, data, error, refresh } = await useFetch(
-  "https://public.radio.co/stations/s2dab303e6/status",
-  {
+  "https://public.radio.co/stations/s2dab303e6/status", {
     lazy: true,
-    server: false,
+    server: false
   }
-);
+)
 
 setInterval(function () {
-  console.log(`refreshing the radio live player ${new Date().toISOString()}`);
-  refresh(); // will call the 'todos' endpoint, just above
-}, 30 * 1000); // 60 * 1000 milsec
+  console.log(`refreshing the radio live player ${new Date().toISOString()}`)
+  refresh() // will call the 'todos' endpoint, just above
+}, 30 * 1000) // 60 * 1000 milsec
 
 const toggleRadio = () => {
   if (!playing.value) {
-    radio.value.load();
-    radio.value.play();
+    radio.value.load()
+    radio.value.play()
   } else {
-    radio.value.pause();
+    radio.value.pause()
   }
-  playing.value = !playing.value;
-};
+  playing.value = !playing.value
+}
 
 onMounted(() => {
-  console.log("mounted");
-  radio.value = new Audio("https://s4.radio.co/s2dab303e6/listen");
-});
+  radio.value = new Audio("https://s4.radio.co/s2dab303e6/listen")
+})
 </script>
